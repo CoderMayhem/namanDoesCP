ee7f9eeeca0b773b5db6cb194b738cab1622e398
CoderMayhem <namanmishra1900@gmail.com>
2021-05-06 20:15:09 +0530

    beans question adding TLE on test 2

---
+++ practice/beans.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+bool canBeDistributed(long long int r, long long int b, long long int d){
+    //base case failure
+    if(d==0 && r!=b){
+        return false;
+    }
+    if(r<=0 || b<=0){
+        return false;
+    }
+
+    if(abs(r-b)<=d){    //base case success
+        return true;
+    }else{              //recursion
+        if(r<b){
+            canBeDistributed(r-1,b-d-1,d);
+        }else if(r>b){
+            canBeDistributed(r-d-1,b-1,d);
+        }
+    }
+}
+
+int main(){
+
+    int t;
+    cin>>t;
+
+    while(t--){
+
+        long long int r,b,d;
+        cin>>r>>b>>d;
+
+        if(canBeDistributed(r,b,d)){
+            cout<<"YES"<<endl;
+        }else
+        {
+            cout<<"NO"<<endl;
+        }
+        
+    }
+
+    return 0;
+
+}
